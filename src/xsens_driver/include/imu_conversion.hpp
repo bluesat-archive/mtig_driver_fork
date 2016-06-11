90fbd4804bb9f34db203242cbcf3865758ac3cc7
hjed <harry@dayfamilyweb.com>
2016-06-11 15:40:36 +1000

    OWRS-195 #time 2h compiling, axis fliped need to do quarternion

--- src/xsens_driver/include/imu_conversion.hpp
+++ src/xsens_driver/include/imu_conversion.hpp
@@ -4,26 +4,26 @@ H file for converting IMU raw to orientation
 
 *******************/
 #include <ros/ros.h>
-#include <geometry_msgs/quaternion.h>
-#include <sensor_msgs/imu.h>
+#include <geometry_msgs/Quaternion.h>
+#include <sensor_msgs/Imu.h>
 
 
-public class imu_conversion {
-public:
-    imu_conversion;
-    geometry_msgs::QuaternionStamped orientation;
-    void publish_quat();//function to publish quaternion
-    void imu_callback();
-    void main_loop();   //prev vals into quats
-    void setup_loop(); //15 sec run, to get averages
-    bool new_params;
-    void quat_convert();
-    void imu_callback(const sensor_msgs::imu::ConstPtr& msg);
+class imu_conversion {
+    public:
+        imu_conversion();
+        geometry_msgs::Quaternion orientation;
+        void publish_quat();//function to publish quaternion
+        void imu_callback();
+        void main_loop();   //prev vals into quats
+        void setup_loop(); //15 sec run, to get averages
+        bool new_params;
+        void quat_convert();
+        void imu_callback(const sensor_msgs::Imu::ConstPtr& msg);
 
-private:
-    ros::NodeHandle nh;
-    ros::publisher quat_msg; //quaternion msg publisher
-    ros::Rate r
-    double[] prev_vals; //x,y,z,r,p,y
+    private:
+        ros::NodeHandle nh;
+        ros::Publisher quatMsgPub; //quaternion msg publisher
+        ros::Rate r;
+        double prev_vals[]; //x,y,z,r,p,y
     
-}
+};
--- src/xsens_driver/src/imu_conversion.cpp
+++ src/xsens_driver/src/imu_conversion.cpp
@@ -13,48 +13,67 @@ conversion to orientation quat can be achieved
 overtime.
 
 ****************************/
+
+/*
+ * Input:
+ *      angular_velocity and linear_acceleration
+ *      orientation on the input is (when xsens word is at the front, top):
+ *              z is going down (z = -z)
+ *              y is positive to the right
+ *              x is positive backwards
+ * Output:
+ *      we want everything in ros's co-ordinate system
+ *      which is x forwards, y is left, z is up
+ */ 
+#include <sensor_msgs/Imu.h>
+#include <imu_conversion.hpp>
+
+static sensor_msgs::Imu flipAxies(sensor_msgs::Imu in);
+
 int main(int argc, char **argv) {
     
     //initialise imu_conversion
-    imu_conversion imu();
+    imu_conversion imu;
 
-    if(ros.ok()) {
+    //we get angular_velocity and linear_acceleration
+    //we want orientation
+    if(ros::ok()) {
         imu.setup_loop();
         imu.main_loop();
     }
 }
 
-imu_conversion::imu_conversion() {
+imu_conversion::imu_conversion() : r(50) {
     new_params = true;
-    ros::Rate r(50);
-    nh.advertise();
-    nh.subscribe("mti/sensor/imu", 0, /*callback*/);
+    //ros::Rate r(50);
+    quatMsgPub =  nh.advertise<geometry_msgs::Quaternion>("/orientation",10,false);
+    nh.subscribe("mti/sensor/imu", 1, &imu_conversion::imu_callback, this); 
 }
 
 void imu_conversion::setup_loop() {
-    double[6] setup_vals;
-    int sample_count = 0;
-    ros::Time setup_begin = ros::Time::now();
-    ros::Time cur = setup_begin;
-    while(cur_time.sec - setup_begin.sec < 15) {
+    double setup_vals[6];
+    int sampleCount = 0;
+    ros::Time setupBegin = ros::Time::now();
+    ros::Time cur = setupBegin;
+    while(cur.sec - setupBegin.sec < 15) {
         r.sleep();
         if(new_params) {
             new_params = false;
             for(unsigned int i = 0; i < 6; ++i) {
                 setup_vals[i] += prev_vals[i]; //angular velocities collected for a bias elimination
-                ++sample_count;
+                ++sampleCount;
             }
         }
         cur = ros::Time::now();
     }
     for(unsigned int i = 0 ; i < 6; ++i) {
-        prev_vals[i] = setup_vals[i]/sample_count;
+        prev_vals[i] = setup_vals[i]/sampleCount;
     }
     //need to convert to quat
 }
 
 void imu_conversion::main_loop() {
-    while(ros.ok()) {    
+    while(ros::ok()) {    
         r.sleep();
         if(new_params) {
             new_params = false;
@@ -65,15 +84,18 @@ void imu_conversion::main_loop() {
     }
 }
 
-void imu_conversion::imu_callback(const sensor_msgs::imu::ConstPtr& msg) {
+void imu_conversion::imu_callback(const sensor_msgs::Imu::ConstPtr& msg) {
+    //flip the axis
+    sensor_msgs::Imu correctMsg = flipAxies(*msg);
+    
     //getting angular velocity & linear acceleration
-    this.prev_vals[0] = msg.linear_acceleration.x;
-    this.prev_vals[1] = msg.linear_acceleration.y;
-    this.prev_vals[2] = msg.linear_acceleration.z;
+    this->prev_vals[0] = correctMsg.linear_acceleration.x;
+    this->prev_vals[1] = correctMsg.linear_acceleration.y;
+    this->prev_vals[2] = correctMsg.linear_acceleration.z;
 
-    this.prev_vals[3] = msg.angular_velocity.x;
-    this.prev_vals[4] = msg.angular_velocity.y;
-    this.prev_vals[5] = msg.angular_velocity.z;
+    this->prev_vals[3] = correctMsg.angular_velocity.x;
+    this->prev_vals[4] = correctMsg.angular_velocity.y;
+    this->prev_vals[5] = correctMsg.angular_velocity.z;
 
     new_params = true;
 }
@@ -83,14 +105,20 @@ void imu_conversion::imu_callback(const sensor_msgs::imu::ConstPtr& msg) {
 //for is orientation.quaternion x,y,z,w 
 void imu_conversion::quat_convert() {
     //defualt yaw to be pi/2 (maybe not though because x is facing axis on ros
+    
 }
 
 
 void imu_conversion::publish_quat() {
-    quat_msg.publish(orientation);
+    quatMsgPub.publish(orientation);
 }
 
-
-
-
-
+static sensor_msgs::Imu flipAxies(sensor_msgs::Imu in) {
+    sensor_msgs::Imu out;
+    out.angular_velocity.x = -in.angular_velocity.x;
+    out.angular_velocity.y = -in.angular_velocity.y;
+    out.angular_velocity.z = -in.angular_velocity.z;
+    out.linear_acceleration.x = -in.linear_acceleration.x;
+    out.linear_acceleration.y = -in.linear_acceleration.y;
+    out.linear_acceleration.z = -in.linear_acceleration.z;
+}
